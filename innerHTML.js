565f3ef103a2105ac0b19776199b0bc0066add4c
Leo Ho <leo.ho@mindera.com>
2022-08-13 10:22:09 +0100

    explained the difference between using DOM elements & innerHTML

---
+++ innerHTML.js
@@ -0,0 +1,6 @@
+// document.body.innerHTML = '<h1>Hello!</h1>';
+
+let h1 = document.createElement('h1');
+let text = document.createTextNode('Hello!');
+// h1.appendChild(text);
+document.body.appendChild(h1);
